5110a376fabde5a4e5908b1bedf0d450c82e50aa
Adam Chevrier <adam.chevrier@gmail.com>
2012-10-10 16:12:28 -0400

    modify actor and game classes to incorporate new Class structure and vector class

--- classes/actor.lua
+++ classes/actor.lua
@@ -2,29 +2,32 @@
 	Actor Class
 --]]------------------------------------------------
 
-Actor = {}
+require('classes.Class')
+require('classes.Vector')
 
-function Actor:new(name, world, x, y)
-	local object = {
-		name = name,
-		world = world,
-		objPoint = {x = x, y = y},
-		selected = false
-	}
+Actor = Class("Actor")
 
-	object.body = love.physics.newBody(world, x, y, "dynamic")
-	object.shape = love.physics.newCircleShape(10)
-	object.fixture = love.physics.newFixture(object.body,object.shape,1)
-	object.fixture:setUserData(object)
-	object.fixture:setRestitution(0.9)
-	object.body:isFixedRotation(false)
-	object.body:setAngle(math.pi/4)
-	object.objTheta = 0
-	object.nametagFont = love.graphics.newFont(10) -- the number denotes the font size
+function Actor:new ( init )
+	init = init or {}
 
-	setmetatable(object, { __index = Actor })  -- Inheritance
+	init.name = init.name or 0
+	init.world = init.world or 0
+	init.objPoint = nil
+	init.selected = false
 
-	return(object)
+	init.body = love.physics.newBody(init.world, init.x, init.y, "dynamic")
+	init.shape = love.physics.newCircleShape(10)
+	init.fixture = love.physics.newFixture(init.body, init.shape, 1)
+	init.fixture:setUserData(init)
+	init.fixture:setRestitution(0.9) --Unitless
+	init.body:isFixedRotation(false)
+	init.body:setAngle(0) --Radians
+	init.objTheta = 0 --Radians
+	init.nametagFont = love.graphics.newFont(10) -- the number denotes the font size
+
+	Actor.super.new(self, init)
+
+	return(init)
 end
 
 function Actor:getName()
@@ -32,8 +35,12 @@ function Actor:getName()
 end
 
 function Actor:setObjective(x,y)
-	self.objPoint.x = x
-	self.objPoint.y = y
+	if(self.objPoint == nil) then
+		self.objPoint = Vector:new({x = x, y = y})
+	else
+		self.objPoint.x = x
+		self.objPoint.y = y
+	end
 end
 
 function Actor:getSelected()
@@ -45,30 +52,35 @@ function Actor:setSelected(lSelected)
 end
 
 function Actor:update()
-	if((math.abs(self.body:getX() - self.objPoint.x) > 5) or
-	   (math.abs(self.body:getY() - self.objPoint.y) > 5)) then
-		local vectorToObj = {x = self.objPoint.x - self.body:getX(), y = self.objPoint.y - self.body:getY()}
-		local magnitude = math.sqrt((math.pow(vectorToObj.x,2)) + (math.pow(vectorToObj.y,2)))
-		local unitVector = {x = vectorToObj.x / math.sqrt(magnitude), y = vectorToObj.y / math.sqrt(magnitude)}
-		local directionUnitVector = {x = math.cos(self.body:getAngle()), y = math.sin(self.body:getAngle())}
-		self.objTheta = math.atan2(directionUnitVector.x * unitVector.y - directionUnitVector.y * unitVector.x, directionUnitVector.x * unitVector.x + directionUnitVector.y * unitVector.y)
-		if(math.abs(self.objTheta) > math.pi / 20) then
-			self.body:applyAngularImpulse(50 * (self.objTheta / math.abs(self.objTheta)))
-			--Need to limit maximum angular velocity!
-		else
-			--I should really use torque to zero-out the angular velocity
-			--so that the body is pointing in the right direction, but
-			--for now I'll cheat.
-			self.body:setAngularVelocity(0)
-			self.body:setAngle(self.body:getAngle() + self.objTheta)
+	if(self.objPoint ~= nil) then
+		if((math.abs(self.body:getX() - self.objPoint.x) > 5) or
+		   (math.abs(self.body:getY() - self.objPoint.y) > 5)) then
+		   	local bodyVector = Vector:new({x = self.body:getX(), y = self.body:getY()})
+			local vectorToObj = self.objPoint - bodyVector
+			local directionUnitVector = {x = math.cos(self.body:getAngle()), y = math.sin(self.body:getAngle())}
 
-			--I'll need to limit maximum linear velocity.
-		   	self.body:applyLinearImpulse(unitVector.x, unitVector.y)
+			self.objTheta = math.atan2(directionUnitVector.x * vectorToObj:unit().y - directionUnitVector.y * vectorToObj:unit().x,
+						   directionUnitVector.x * vectorToObj:unit().x + directionUnitVector.y * vectorToObj:unit().y)
+
+			if(math.abs(self.objTheta) > math.pi / 20) then
+				self.body:applyAngularImpulse(50 * (self.objTheta / math.abs(self.objTheta)))
+				--Need to limit maximum angular velocity!
+			else
+				--I should really use torque to zero-out the angular velocity
+				--so that the body is pointing in the right direction, but
+				--for now I'll cheat.
+				self.body:setAngularVelocity(0)
+				self.body:setAngle(self.body:getAngle() + self.objTheta)
+	
+				--I'll need to limit maximum linear velocity.
+				self.body:applyLinearImpulse(vectorToObj:unit().x, vectorToObj:unit().y)
+			end
+		else
+			--This is where I need to properly apply force to counter the linear velocity.
+			--For now I'll cheat
+			self.body:setLinearVelocity(0,0) --comment this out for rubberband funtimes
+			self.objPoint = nil
 		end
-	else
-		--This is where I need to properly apply force to counter the linear velocity.
-		--For now I'll cheat
-		self.body:setLinearVelocity(0,0) --comment this out for rubberband funtimes
 	end
 end
 
@@ -76,36 +88,45 @@ function Actor:draw()
 	--grab numbers I'm going to use a lot
 	local bodyAngle = self.body:getAngle()
 	local shapeRadius = self.shape:getRadius()
-	local bodyWorldX = self.body:getX()
-	local bodyWorldY = self.body:getY()
+	local bodyWorldPos = Vector:new({x = self.body:getX(), y = self.body:getY()})
 
-	--Line to objPoint
-	love.graphics.setColor(255,192,203)
-	love.graphics.line(bodyWorldX, bodyWorldY, self.objPoint.x, self.objPoint.y)
+	if(self.objPoint ~= nil) then
+		--Line to objPoint
+		love.graphics.setColor(255,192,203)
+		love.graphics.line(bodyWorldPos.x, bodyWorldPos.y, self.objPoint.x, self.objPoint.y)
 
-	--Direction to objPoint indicator
-	love.graphics.setColor(128,0,0)
-	love.graphics.line(bodyWorldX, bodyWorldY, bodyWorldX + (shapeRadius * math.cos(bodyAngle + self.objTheta)), bodyWorldY + (shapeRadius * math.sin(bodyAngle + self.objTheta)))
+		--Direction to objPoint indicator
+		love.graphics.setColor(128,0,0)
+		love.graphics.line(bodyWorldPos.x, bodyWorldPos.y, bodyWorldPos.x + (shapeRadius * math.cos(bodyAngle + self.objTheta)), bodyWorldPos.y + (shapeRadius * math.sin(bodyAngle + self.objTheta)))
+	end
 
 	--Facing indicator
 	love.graphics.setColor(210,180,140)
-	love.graphics.circle("line", bodyWorldX, bodyWorldY, shapeRadius,50)
-	love.graphics.line(bodyWorldX, bodyWorldY, bodyWorldX + (shapeRadius * math.cos(bodyAngle)), bodyWorldY + (shapeRadius * math.sin(bodyAngle)))
+	love.graphics.circle("line", bodyWorldPos.x, bodyWorldPos.y, shapeRadius,50)
+	love.graphics.line(bodyWorldPos.x, bodyWorldPos.y, bodyWorldPos.x + (shapeRadius * math.cos(bodyAngle)), bodyWorldPos.y + (shapeRadius * math.sin(bodyAngle)))
+
+	--Velocity Vector
+	love.graphics.setColor(113, 188, 120) --Fern Green
+	local linVelX, linVelY = self.body:getLinearVelocity()
+	local linVel = Vector:new({x = linVelX, y = linVelY})
+	linVel = linVel + bodyWorldPos
+	love.graphics.line(bodyWorldPos.x, bodyWorldPos.y, linVel.x, linVel.y)
+	--Maybe a nice little arrowhead?
 
 	--NameTag
 	love.graphics.setColor(153,153,255)
 	love.graphics.setFont(self.nametagFont)
-	love.graphics.print(self.name, bodyWorldX - (self.nametagFont:getWidth(self.name)/2), bodyWorldY - (self.nametagFont:getHeight() + 15),0,1,1,0,0,0,0)
+	love.graphics.print(self.name, bodyWorldPos.x - (self.nametagFont:getWidth(self.name)/2), bodyWorldPos.y - (self.nametagFont:getHeight() + 15),0,1,1,0,0,0,0)
 
 	--print objTheta
-	--love.graphics.print(math.deg(self.objTheta), bodyWorldX - (self.nametagFont:getWidth(math.deg(self.objTheta))/2), bodyWorldY + (self.nametagFont:getHeight() + 15),0,1,1,0,0,0,0)
+	--love.graphics.print(math.deg(self.objTheta), bodyWorldPos.x - (self.nametagFont:getWidth(math.deg(self.objTheta))/2), bodyWorldPos.y + (self.nametagFont:getHeight() + 15),0,1,1,0,0,0,0)
 	
 	--Selection Box
-	--love.graphics.print(tostring(self.selected), bodyWorldX - (self.nametagFont:getWidth(tostring(self.selected)) / 2), bodyWorldY + (self.nametagFont:getHeight()),0,1,1,0,0,0,0)
+	--love.graphics.print(tostring(self.selected), bodyWorldPos.x - (self.nametagFont:getWidth(tostring(self.selected)) / 2), bodyWorldPos.y + (self.nametagFont:getHeight()),0,1,1,0,0,0,0)
 	if(self.selected == true) then
 		love.graphics.setColor(255,255,255)
 		local topLeftX, topLeftY, bottomRightX, bottomRightY = self.shape:computeAABB( 0, 0, self.body:getAngle(), 1 )
-		love.graphics.rectangle("line", bodyWorldX - math.abs(topLeftX), bodyWorldY - math.abs(topLeftY), (bottomRightX - topLeftX), (bottomRightY - topLeftY))
+		love.graphics.rectangle("line", bodyWorldPos.x - math.abs(topLeftX), bodyWorldPos.y - math.abs(topLeftY), (bottomRightX - topLeftX), (bottomRightY - topLeftY))
 	end
 	
 end
