ad35701a7355895bba95db56882b527a8e345eb7
John Stahara <john.stahara@gmail.com>
2012-10-25 17:49:15 -0400

    Remove redundant points from paths upon expansion.

--- classes/Actor.lua
+++ classes/Actor.lua
@@ -108,6 +108,21 @@ end
 
 --[[
 -- ===  METHOD  ========================================================================
+--    Signature:  Actor:move_at_step ( ) -> table|nil
+--  Description:  Obtain the movement command at the given step in this actor's move
+--                list.
+--   Parameters:  step : [number] : step index of move to be returned
+--      Returns:  Movement description object or nil if no move exists at the given
+--                step.
+-- =====================================================================================
+--]]
+function Actor:move_at_step ( step )
+	return self.path.moves[self.path.step]
+end
+
+
+--[[
+-- ===  METHOD  ========================================================================
 --    Signature:  Actor:current_move ( ) -> table|nil
 --  Description:  Obtain this actor's current movement command.
 --      Returns:  Movement description object or nil if the move list is empty.
@@ -115,6 +130,18 @@ end
 --]]
 function Actor:current_move ( )
 	return self.path.moves[self.path.step]
+end
+
+
+--[[
+-- ===  METHOD  ========================================================================
+--    Signature:  Actor:current_move ( ) -> table|nil
+--  Description:  Obtain this actor's current movement command.
+--      Returns:  Movement description object or nil if there is no 'next' move.
+-- =====================================================================================
+--]]
+function Actor:next_move ( )
+	return self.path.moves[self.path.step+1]
 end
 
 
@@ -149,25 +176,72 @@ end
 
 --[[
 -- ===  METHOD  ========================================================================
---    Signature:  Actor:expand_current_move ( ) -> nil
+--    Signature:  Actor:expand_move_at_step ( step ) -> table
+--  Description:  Update this actor's move list by expanding the specified move, if
+--                necessary.
+--   Parameters:  step - [number] - step index of move to be expanded
+--      Returns:  First move of expanded list.
+-- =====================================================================================
+--]]
+function Actor:expand_move_at_step ( step )
+	local move = self.path.moves[step]
+
+	if move and move.type == 'path' then
+		local x = step <= 1 and self.loc.x() or self.path.moves[step-1].dest.x
+		local y = step <= 1 and self.loc.y() or self.path.moves[step-1].dest.y
+		local path_moves = self.world:path(self.loc.x(), self.loc.y(), move.dest.x, move.dest.y) or {}
+
+		self.path.moves[step] = nil
+
+		local s = 0
+		for i, m in ipairs(path_moves) do
+			local clear = true
+
+			if i > 1 and step + s > 2 then
+				local m2 = self.path.moves[step + s - 2].dest
+				local clear = true
+
+				self.world.physicsWorld:rayCast(m2.x, m2.y, m.x, m.y, function() clear = false; return 0 end)
+
+				if clear then
+					s = s - 1
+					local dest = self.path.moves[step + s].dest
+					table.remove(self.path.moves, step + s)
+				end
+			end
+
+			table.insert(self.path.moves, step + s, self.line_to(m.x, m.y))
+			s = s + 1
+		end
+	end
+
+	return self:move_at_step(step);
+end
+
+
+--[[
+-- ===  METHOD  ========================================================================
+--    Signature:  Actor:expand_current_move ( ) -> table
 --  Description:  Update this actor's move list by expanding the current move, if
 --                necessary.
+--      Returns:  First step of the current move, after expansion.
 -- =====================================================================================
 --]]
 function Actor:expand_current_move ( )
-	local move = self:current_move()
+	return self:expand_move_at_step(self.path.step)
+end
 
-	if move and move.type == 'path' then
-		local x = self.path.step <= 1 and self.loc.x() or self.path.moves[self.path.step-1].dest.x
-		local y = self.path.step <= 1 and self.loc.y() or self.path.moves[self.path.step-1].dest.y
-		local path_moves = self.world:path(self.loc.x(), self.loc.y(), move.dest.x, move.dest.y) or {}
 
-		self.path.moves[self.path.step] = nil
-
-		for i, m in ipairs(path_moves) do
-			table.insert(self.path.moves, self.path.step + i - 1, Actor.line_to(m.x, m.y))
-		end
-	end
+--[[
+-- ===  METHOD  ========================================================================
+--    Signature:  Actor:expand_next_move ( ) -> table
+--  Description:  Update this actor's move list by expanding the next move, if
+--                necessary.
+--      Returns:  First step of next move, after expansion.
+-- =====================================================================================
+--]]
+function Actor:expand_next_move ( )
+	return self:expand_move_at_step(self.path.step + 1)
 end
 
 
@@ -179,17 +253,12 @@ end
 -- =====================================================================================
 --]]
 function Actor:expand_all_moves ( )
-	local s = self.path.step
+	local s = 1
 
-
-	self.path.step = 1
-	while self:current_move() do
-		self:expand_current_move()
-		self.path.step = self.path.step + 1
+	while self:move_at_step(s) do
+		self:expand_move_at_step(s)
+		s = s + 1
 	end
-
-
-	self.path.step = s
 end
 
 
@@ -229,34 +298,100 @@ end
 ====================================================================================
 --]]
 
+
+function Actor:dist_to_next_move ( )
+	local move = self:current_move()
+	return Vector.mag({x = move.dest.x - self.loc.x(), y = move.dest.y - self.loc.y()})
+end
+
+
+function Actor:time_to_next_move ( )
+	return self.body:getLinearVelocity() / self:dist_to_next_move()
+end
+
+
 function Actor:update ( )
 	local move = self:current_move()
 
+	-- Stop at the end of the move list.
 	if not move then
+		self.path.step = 0
+
+		self.force.x = 0
+		self.force.y = 0
+
+		self.body:setLinearVelocity(0, 0)
+		self.body:setAngularVelocity(0)
+
+		self.turning = false
+		self.moving = false
+
 		return
 	end
 
 	self:expand_current_move()
 
-	-- Get the next move when the current one is completed.
-	if Vector.mag({x = move.dest.x - self.loc.x(), y = move.dest.y - self.loc.y()}) < 0.5 then
-		self.path.step = self.path.step + 1
-		move = self:current_move()
-		self:expand_current_move()
 
-		-- Stop at the end of the move list.
-		if not move then
-			self.path.step = 0
-			move = nil
+	local dest_heading = Vector(move.dest.x - self.loc.x(), move.dest.y - self.loc.y())
+	local dest_angle = math.atan2(dest_heading.y, dest_heading.x)
 
-			self.force.x = 0
-			self.force.y = 0
-			self.body:setLinearVelocity(0, 0)
-			self.body:setAngularVelocity(0)
+	self.deflection = dest_angle - self.angle()
 
-			return
+
+	local time_to_next = self:time_to_next_move()
+	local next_move = self:next_move()
+
+	if false and next_move and not self.turning and time_to_next < 1 then
+		local next_heading = Vector(next_move.dest.x - move.dest.x, next_move.dest.y - move.dest.y)
+		local next_angle = math.atan2(next_heading.y, next_heading.x)
+		local next_deflection = next_angle - self.angle()
+
+		if time_to_next < 1 then
+			self.body:applyAngularImpulse(next_deflection * self.body:getMass())
+			self.turning = true
 		end
 	end
+
+
+	-- Get the next move when the current one is completed.
+	if not self.moving or self:dist_to_next_move() < 0.5 then
+		self.path.step = self.path.step + 1
+		self.turning = false
+
+
+		self.body:setAngle(dest_angle)
+		self.force = dest_heading:unit() * self.max_accel
+		self.body:setLinearVelocity(self.force.x, self.force.y)
+		self.moving = true
+	end
+end
+
+function Actor:update ( )
+	local move = self:current_move()
+
+	-- Get the next move when the current one is completed.
+	if move and Vector.mag({x = move.dest.x - self.loc.x(), y = move.dest.y - self.loc.y()}) < 0.5 then
+		self.path.step = self.path.step + 1
+		move = self:current_move()
+	end
+
+	-- Stop at the end of the move list.
+	if not move then
+		self.path.step = 0
+		move = nil
+
+		self.force.x = 0
+		self.force.y = 0
+		self.body:setLinearVelocity(0, 0)
+		self.body:setAngularVelocity(0)
+
+		self.turning = false
+		self.moving = false
+
+		return
+	end
+
+	self:expand_current_move()
 
 	local dest_heading = Vector(move.dest.x - self.loc.x(), move.dest.y - self.loc.y())
 	local dest_angle = math.atan2(dest_heading.y, dest_heading.x)
